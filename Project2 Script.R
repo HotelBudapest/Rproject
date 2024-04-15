76452fe951c443b71bfc4f546de201b3c4f1ee7b
HotelBudapest <arianislam.010@gmail.com>
2024-04-15 20:46:33 +1000

    updates Script

--- Project2 Script.R
+++ Project2 Script.R
@@ -5,8 +5,18 @@ data = read.csv("untitled form.csv")
 
 new_name = c("timestamp", "commute", "exercise", "intensity", "employment", "workingHours", "gender", "sleep", "screenTime")
 colnames(data) = new_name
-str(data)
+
 library(tidyverse)
-ggplot(data, aes(x = exercise, y = workingHours, color = intensity)) +
-  geom_point() +
-  labs(x = "hours exercised", y = "hourse worked", color = "Legend")
+
+ggplot(data, aes(sleep ,intensity)) + 
+  geom_boxplot(alpha = 0.7, color = "black")+    
+  labs(title = "Hours Slept at Night by Exercise Intensity",
+       x = "Hours Slept at Night",
+       y = "Intensity of Exercise") 
+
+ggplot(data, aes(sleep, fill = intensity)) +
+  geom_histogram(binwidth = 2, alpha = 0.8, position = "stack")+ 
+  labs(title = "Histogram of Hours Slept at Night by Exercise Intensity",
+       x = "Hours Slept at Night",
+       y = "Frequency") +
+  theme_minimal()
